8ef5ada3fb93760e5956a5681f200df22a9c8c44
BLARD <peteromu76@email.com>
2025-02-04 01:30:43 +0100

    cal logic

--- src/helper/classifyNumber.ts
+++ src/helper/classifyNumber.ts
@@ -5,6 +5,38 @@ import { isPrime, isPerfect, isArmstrong } from './index';
 
 dotenv.config();
 
+// export const classifyNumber = async (num: number) => {
+//     const properties: string[] = [];
+//     if (isPrime(num)) properties.push("prime");
+//     if (isPerfect(num)) properties.push("perfect");
+//     if (isArmstrong(num)) properties.push("armstrong");
+//     if (num % 2 === 0) properties.push("even");
+//     else properties.push("odd");
+
+//     // const digitSum = num.toString().split('').reduce((sum, digit) => sum + parseInt(digit), 0);
+//     const digits = num.toString().split('').map(Number);
+// const digitSum = digits.reduce((sum, digit) => sum + digit, 0);
+
+
+//     let funFact = "";
+//     try {
+//         const response = await axios.get(`${process.env.API_URL}/${num}`);
+//         funFact = response.data;
+//     } catch (error) {
+//         funFact = "Could not fetch fun fact...";
+//     }
+
+//     return {
+//         number: num,
+//         is_prime: isPrime(num),
+//         is_perfect: isPerfect(num),
+//         properties,
+//         digit_sum: digitSum,
+//         fun_fact: funFact,
+//     };
+// };
+
+
 export const classifyNumber = async (num: number) => {
     const properties: string[] = [];
     if (isPrime(num)) properties.push("prime");
@@ -13,7 +45,10 @@ export const classifyNumber = async (num: number) => {
     if (num % 2 === 0) properties.push("even");
     else properties.push("odd");
 
-    const digitSum = num.toString().split('').reduce((sum, digit) => sum + parseInt(digit), 0);
+    const digitSum = num < 0 
+    ? -Math.abs(num).toString().split('').reduce((sum, digit) => sum + parseInt(digit), 0)
+    : num.toString().split('').reduce((sum, digit) => sum + parseInt(digit), 0);
+
 
     let funFact = "";
     try {
@@ -31,4 +66,5 @@ export const classifyNumber = async (num: number) => {
         digit_sum: digitSum,
         fun_fact: funFact,
     };
-};
+
+}
